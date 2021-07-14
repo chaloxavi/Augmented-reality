20a7f6f9188f3b91b2870dcf1dfd77a0e23f0275
Gonza <chalovaxi@hotmail.com>
2021-07-13 22:42:03 -0500

    FEATURE: INITIAL PROJECT

---
+++ Assets/ExtenderTraker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyAR;
+
+
+public class ExtenderTraker : MonoBehaviour {
+
+	public GameObject Target;
+	public GameObject ContainerTarget;
+	public float timeActiveOnTargetlose = .5f;
+
+
+	bool detected= false;
+	float timer=0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Target.activeSelf == true) {
+			detected = true;
+			timer = 0f;
+		} else {
+		
+			detected = false;
+		
+		}
+
+		if (Target.activeSelf == false && timer < timeActiveOnTargetlose) {
+			ContainerTarget.transform.SetParent (this.transform);
+			timer += Time.deltaTime;
+		} else if(Target.activeSelf==false){
+			ContainerTarget.transform.SetParent (Target.transform);
+		}
+		
+	}
+}
---
+++ Assets/control.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour {
+	public void camEsc(string nombre){
+		SceneManager.LoadScene (nombre);
+	}
+
+	public void salir(){
+		Application.Quit ();
+	
+	}
+}
